42fa8ce9f5d63cff11dc7a2b6a00a54e356fdc95
Gustavo Alencar Silva <gustavoalens@gmail.com>
2023-09-18 21:04:07 -0300

    Add search, insert, remove and contains + tests

--- Sources/BArray/BArray.swift
+++ Sources/BArray/BArray.swift
@@ -1,6 +1,130 @@
-public struct BArray {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class BArray<BData: BArrayData> {
+    private(set) var storedData: [BData]
+    
+    public init(initialValues: [BData]) {
+        storedData = initialValues
+    }
+    
+    public subscript(id: BData.Comp) -> BData? {
+        get { getItem(byID: id) }
+        
+        set {
+            guard let newValue: BData = newValue else { return }
+            guard let index: Int = getIndex(byID: id) else {
+                insert(newValue)
+                return
+            }
+            storedData[index] = newValue
+        }
+    }
+    
+    func insert(_ newItem: BData) {
+        let (index, updateItem) = getNextIndex(byReference: newItem.id)
+        guard !updateItem else {
+            storedData[index] = newItem
+            return
+        }
+        storedData.insertOrAppend(newItem, at: index)
+    }
+    
+    func insert(contentsOf items: [BData]) {
+        items.forEach { insert($0) }
+    }
+    
+    @discardableResult
+    func remove(byID id: BData.Comp) -> BData? {
+        guard let index = getIndex(byID: id) else { return nil }
+        return storedData.remove(at: index)
+    }
+    
+    @discardableResult
+    func remove(item: BData) -> BData? { remove(byID: item.id) }
+    
+    func contains(item: BData) -> Bool { return getIndex(byID: item.id) != nil }
+    
+    func contains(where handler: (BData) -> Bool) -> Bool {
+        return storedData.contains(where: handler)
+    }
+    
+    private func getIndex(byID id: BData.Comp) -> Int? {
+        var lower = 0
+        var upper = storedData.count - 1
+        while lower <= upper {
+            let mid: Int = (upper + lower) / 2
+            let actual: BData = storedData[mid]
+            
+            if actual.id == id { return mid }
+            if storedData[lower].id == id { return lower }
+            if storedData[upper].id == id { return upper }
+            if actual.id > id {
+                lower += 1
+                upper = mid - 1
+            } else {
+                lower = mid + 1
+                upper -= 1
+            }
+        }
+        return nil
+    }
+    
+    private func getItem(byID id: BData.Comp) -> BData? {
+        guard let index: Int = getIndex(byID: id) else {
+            return nil
+        }
+        return storedData[index]
+    }
+    
+    private func getNextIndex(byReference id: BData.Comp) -> (Int, Bool) {
+        var lower = 0
+        var upper = storedData.count - 1
+        var mid: Int = (upper + lower) / 2
+        while lower <= upper {
+            mid = (upper + lower) / 2
+            let actual: BData = storedData[mid]
+            
+            if actual.id == id { return (mid, true) }
+            if storedData[lower].id > id { return (lower, false) }
+            if storedData[upper].id < id { return (upper + 1, false) }
+            if actual.id > id {
+                lower += 1
+                upper = mid - 1
+            } else {
+                lower = mid + 1
+                upper -= 1
+            }
+        }
+        return (storedData[mid].id < id ? mid + 1 : mid, false)
+    }
+}
+
+public protocol BArrayData {
+    associatedtype Comp: Comparable
+    var id: Comp { get }
+}
+
+extension Array {
+    public mutating func insertOrAppend(_ newItem: Element, at i: Int) {
+        guard i < count else {
+            append(newItem)
+            return
+        }
+        guard i >= 0 else {
+            insert(newItem, at: 0)
+            return
+        }
+        insert(newItem, at: i)
+    }
+}
+
+extension Array where Element: Comparable {
+    func isSorted(isOrderedBefore: (Element, Element) -> Bool = { $0 < $1 }) -> Bool {
+        for i in 1..<self.count {
+            if !isOrderedBefore(self[i-1], self[i]) {
+                return false
+            }
+        }
+        return true
     }
 }
--- Tests/BArrayTests/BArrayTests.swift
+++ Tests/BArrayTests/BArrayTests.swift
@@ -1,11 +1,103 @@
 import XCTest
 @testable import BArray
 
-final class BArrayTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BArray().text, "Hello, World!")
+class BArrayTests: XCTestCase {
+    
+    let batchSize = 1000000
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+    
+    func testSearchValue() throws {
+        let data = getRandomBArray()
+        data.refValues.enumerated().forEach { offset, value in
+            XCTAssertEqual(value, data.bArray[value])
+        }
     }
+    
+    func testInsert() throws {
+        let data = getSequentialBArray(batchSize: 100, multiplier: 10)
+        data.refValues.forEach { refValue in
+            let valueToInsert = refValue + 5
+            data.bArray.insert(valueToInsert)
+            XCTAssertTrue(data.bArray.storedData.isSorted())
+        }
+        data.refValues.forEach { refValue in
+            let valueToInsert = refValue - 2
+            data.bArray.insert(valueToInsert)
+            XCTAssertTrue(data.bArray.storedData.isSorted())
+        }
+    }
+    
+    func testRemove() throws {
+        var data = getRandomBArray(batchSize: 10000)
+        data.refValues.remove(at: 0)
+        var range = 0..<data.refValues.count
+        while !range.isEmpty {
+            let index = Int.random(in: range)
+            let valueToRemove = data.refValues.remove(at: index)
+            data.bArray.remove(item: valueToRemove)
+            XCTAssertTrue(!data.bArray.storedData.contains(valueToRemove) && data.bArray.storedData.isSorted())
+            range = 0..<data.refValues.count
+        }
+
+    }
+    
+    func testSearchPerformance() throws {
+        let (data, refValues) = getPerfomanceData()
+        self.measure {
+            for refValue in refValues {
+                _ = data.bArray[refValue]
+            }
+        }
+    }
+    
+    func testPerfomanceComparable() throws {
+        let (data, refValues) = getPerfomanceData()
+        self.measure {
+            for refValue in refValues {
+                for value in data.refValues {
+                    if value == refValue {
+                        return
+                    }
+                }
+            }
+        }
+    }
+    
+    private func getRandomBArray(batchSize size: Int? = nil) -> (refValues: [Int], bArray: BArray<Int>) {
+        var setValues: Set<Int> = Set()
+        while setValues.count < size ?? batchSize {
+            setValues.insert(Int.random(in: 0...Int.max))
+        }
+        let initialValues = setValues.sorted()
+        return (initialValues, BArray(initialValues: initialValues))
+    }
+    
+    private func getSequentialBArray(batchSize size: Int? = nil, multiplier: Int = 1) -> (refValues: [Int], bArray: BArray<Int>) {
+        let batchSize = size ?? batchSize
+        var initialValues: [Int] = Array(1...batchSize)
+        if multiplier > 1 {
+            initialValues = initialValues.map { $0 * multiplier }
+        }
+        return (initialValues, BArray(initialValues: initialValues))
+    }
+    
+    private func getPerfomanceData() -> (data: (refValues: [Int], bArray: BArray<Int>), refValues: [Int]) {
+        let batchSize = 10000000
+        let data = getSequentialBArray(batchSize: batchSize)
+        let refValues: [Int] = Array(repeating: [1000000, 12500000, 2000000, 5000000, 8000000, 9999999, 10000000], count: 1000).reduce([]) { partialResult, part in
+            return partialResult + part
+        }
+        return (data, refValues)
+    }
+    
+}
+
+extension Int: BArrayData {
+    public var id: Int { self }
 }
