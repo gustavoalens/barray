d8076f320120ab765a00f96d9828181bd7d0d547
Gustavo Alencar Silva <gustavoalens@gmail.com>
2023-10-01 17:50:34 -0300

    Remove comments

--- Tests/BArrayTests/BArrayTests.swift
+++ Tests/BArrayTests/BArrayTests.swift
@@ -4,13 +4,6 @@ import XCTest
 class BArrayTests: XCTestCase {
     
     let batchSize = 1000000
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
     
     func testSearchValue() throws {
         let data = getRandomBArray()
